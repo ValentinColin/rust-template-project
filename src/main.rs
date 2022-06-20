8d8d93306f90b9efe5a261049ed4e62d8bb3d6ef
Valentin Colin <valentin.colin78@gmail.com>
2022-06-20 18:17:04 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+//! Description of this crate
+//!
+//! Careful to the syntax:
+//!
+//! |               | Inner         | Outer         |
+//! |---------------|:-------------:|:-------------:|
+//! | Line          | //! blabla    | /// blabla    |
+//! | Block         | /*! blabla */ | /** blabla */ |
+//! | attribute     | #![attr]      | #[attr]       |
+//!
+//! - Inner attribute: #![attr]
+//! - Outer attribute: #[attr]
+
+// Good pratice: use these attributes
+#![deny(missing_docs,
+        missing_debug_implementations,
+        missing_copy_implementations,
+        trivial_casts,
+        trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces,
+        unused_qualifications)]
+
+/// This function returns the greeting: `Hello, world!`
+fn main() {
+    println!("Hello World!");
+}
