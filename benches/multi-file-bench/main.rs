be1246f7c3703eeb56dae9067e69081ca1d30898
Valentin Colin <valentin.colin78@gmail.com>
2022-08-05 10:29:59 +0200

    lint

--- benches/multi-file-bench/main.rs
+++ benches/multi-file-bench/main.rs
@@ -3,10 +3,10 @@
 #![feature(test)]
 
 extern crate test;
-use test::{Bencher, black_box};
+use test::{black_box, Bencher};
 
 mod bench_module;
-use bench_module::{fibonacci_slow, fibonacci_fast};
+use bench_module::{fibonacci_fast, fibonacci_slow};
 
 #[bench]
 fn fibo_slow_10(b: &mut Bencher) {
@@ -21,7 +21,6 @@ fn fibo_fast_10(b: &mut Bencher) {
         black_box(fibonacci_fast(10));
     });
 }
-
 
 #[bench]
 fn fibo_fast_100(b: &mut Bencher) {
