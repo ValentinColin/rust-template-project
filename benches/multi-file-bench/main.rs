d52ffa1a317f896f1de936ca9bada1917f84aa0a
Valentin Colin <valentin.colin78@gmail.com>
2022-08-05 10:39:00 +0200

    remove big bench

--- benches/multi-file-bench/main.rs
+++ benches/multi-file-bench/main.rs
@@ -21,17 +21,3 @@ fn fibo_fast_10(b: &mut Bencher) {
         black_box(fibonacci_fast(10));
     });
 }
-
-#[bench]
-fn fibo_fast_100(b: &mut Bencher) {
-    b.iter(|| {
-        black_box(fibonacci_fast(100));
-    });
-}
-
-#[bench]
-fn fibo_fast_1000(b: &mut Bencher) {
-    b.iter(|| {
-        black_box(fibonacci_fast(1000));
-    });
-}
